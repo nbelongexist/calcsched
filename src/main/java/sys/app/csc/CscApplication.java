5225aa81e01c68bd26a5fb6c3e526302047e1f26
Nehemias <nbelong@exists.com>
2022-08-31 08:28:03 +0800

    first commit

---
+++ src/main/java/sys/app/csc/CscApplication.java
@@ -0,0 +1,13 @@
+package sys.app.csc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sys/app/csc/CscApplicationTests.java
@@ -0,0 +1,13 @@
+package sys.app.csc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
